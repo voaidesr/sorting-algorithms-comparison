03e1cf90b9e7a2226e86e8dee9ccc546e061c9fd
Voaides Robert <voaides.r@yahoo.com>
2025-04-17 02:09:17 +0300

    latex presentation

--- results.py
+++ results.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
+import time
 
 df = pd.read_csv('results.csv')
 
@@ -9,4 +11,7 @@ plt.xlabel('Time (seconds)')
 plt.title('Comparison of Sorting Algorithms')
 plt.grid(True)
 plt.tight_layout()
-plt.show()
+save_folder = "./prezentare/img"
+timestamp = time.strftime("%Y%m%d_%H%M%S")
+filename = os.path.join(save_folder, f"img_{timestamp}.png")
+plt.savefig(filename)
